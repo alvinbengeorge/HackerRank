9f512406ef73307a4aec2ddcdf2a01016fcd2ddd
alvinbengeorge <alvinallen333@gmail.com>
2021-10-28 08:19:37 +0530

    Added 1st

--- 1.py
+++ 1.py
@@ -1,6 +1,6 @@
 #https://www.hackerrank.com/challenges/python-arithmetic-operators/problem?isFullScreen=true
 # 
-# if __name__ == '__main__':
+if __name__ == '__main__':
     a = int(input())
     b = int(input())
     print(a+b)
