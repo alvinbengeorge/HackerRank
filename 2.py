4400b6f548d168fd15b4e7bbcea384cd5c1e4edf
alvinbengeorge <alvinallen333@gmail.com>
2021-10-28 08:23:00 +0530

    Finished 2nd question

---
+++ 2.py
@@ -0,0 +1,5 @@
+#https://www.hackerrank.com/challenges/python-loops/problem?isFullScreen=true&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen
+
+if __name__ == '__main__':
+    n = int(input())
+    [print(i*i) for i in range(n)]
